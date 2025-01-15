aec1a76a1e5803edf6d0f3e2864f0fb798a3b2d6
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-01-15 10:48:00 +0100

    Update

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -76,12 +76,12 @@
             </div>
             <div class="col-lg-1"></div>
             <div class="col-lg-7 mb-4">
-            <p class="E">
-                Experience the power of sustainable living with Baltai Energy. Our cutting-edge solar solutions deliver reliable, cost-effective, and eco-friendly energy tailored to your needs. Make the switch today and illuminate your world!
-            </p>
-            <div class="my-4">
-                <button class="btn text-white px-4 py-2">Request a free quote</button>
-            </div>
+                <p class="E">
+                    Experience the power of sustainable living with Baltai Energy. Our cutting-edge solar solutions deliver reliable, cost-effective, and eco-friendly energy tailored to your needs. Make the switch today and illuminate your world!
+                </p>
+                <div class="my-4">
+                    <button class="btn text-white px-4 py-2">Request a free quote</button>
+                </div>
             </div>
             <div class="row g-2 mt-5">
                 <!-- Card 1 -->
@@ -166,8 +166,103 @@
 <!-- Benefit Ends -->
 
 <!-- FAQs -->
-
+<main class="faqPage" id="FAQs">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                
+                <h1>Why Choose Baltai Energy?</h1>
+                <p>
+                Experience the power of sustainable living with Baltai Energy. Our cutting-edge solar solutions deliver reliable, cost-effective, and eco-friendly energy
+                </p>
+            </div>
+            <div class="col-lg-6"></div>
+            <div class="col-lg-6">
+                <div class="faq-box">
+                    <details>
+                        <summary>Best Quality Equipment?</summary>
+                        <div class="faq-content">
+                            <p> We deploy only best-in-class products with EU standards of compliance, and our solutions are tailored to adequately meet your commercial and domestic capacity needs hassle-free. You’ll find that our installation set-ups with specified panel-inverter-Battery combinations are unique, easy to install, and come with an aesthetically pleasing industrial finish. </p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary>Professional Expertise?</summary>
+                        <div class="faq-content">
+                            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis adipisci aliquid id atque repudiandae ullam molestias quibusdam impedit. Nemo iste repellat obcaecati iure ad nobis. Nemo cum sunt rem. Recusandae beatae autem animi tenetur, vitae harum. Consequuntur itaque quod dicta unde totam quas pariatur? Dolores, tempore. Fugit magni ex vel. </p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary> Tailor-Made Solutions </summary>
+                        <div class="faq-content">
+                            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis adipisci aliquid id atque repudiandae ullam molestias quibusdam impedit. Nemo iste repellat obcaecati iure ad nobis. Nemo cum sunt rem. Recusandae beatae autem animi tenetur, vitae harum. Consequuntur itaque quod dicta unde totam quas pariatur? Dolores, tempore. Fugit magni ex vel. </p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary> Why Choose Baltai Energy? </summary>
+                        <div class="faq-content">
+                            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis adipisci aliquid id atque repudiandae ullam molestias quibusdam impedit. Nemo iste repellat obcaecati iure ad nobis. Nemo cum sunt rem. Recusandae beatae autem animi tenetur, vitae harum. Consequuntur itaque quod dicta unde totam quas pariatur? Dolores, tempore. Fugit magni ex vel. </p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary> Tailor-Made Solutions </summary>
+                        <div class="faq-content">
+                            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis adipisci aliquid id atque repudiandae ullam molestias quibusdam impedit. Nemo iste repellat obcaecati iure ad nobis. Nemo cum sunt rem. Recusandae beatae autem animi tenetur, vitae harum. Consequuntur itaque quod dicta unde totam quas pariatur? Dolores, tempore. Fugit magni ex vel. </p>
+                        </div>
+                    </details>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <img src="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736926675/Energy/11-06010c40-unscreen-ezgif.com-optimize_ryupcx.gif" draggable="false" alt="">
+            </div>
+        </div>
+    </div>
+</main>
 <!-- FAQs  Ends-->
+
+
+<!-- Providing -->
+<section class="custom-section">
+    <div class="">
+        <div class="row align-items-center">
+            <div class="col-lg-1"></div>
+            <!-- Left Content -->
+            <div class="col-lg-5 Justt ">
+                <p class="quote-text">“Leading Sustainable Energy Provider”</p>
+                <h1 class="main-heading">Providing Innovative and Sustainable Solutions</h1>
+                <p>
+                    We are a Solar Systems Solutions Provider based in Lagos, Nigeria; with an international team
+                    and exclusive partnerships with top European and Asian Engineering OEMs. We provide Off-grid and
+                    On-grid solutions that cover every capacity or scale: from residential, to commercial, to
+                    industrial applications.
+                </p>
+                <div class="row mt-4">
+                    <div class="col-md-4 icon-text">
+                        <img src="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736931550/Energy/Rectangle_18_psqrxy.png" alt="Sustainability Icon">
+                        <p class="mt-2">Environmental <br> Sustainability</p>
+                    </div>
+                    <div class="col-md-4 icon-text">
+                        <img src="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736931550/Energy/Rectangle_32_fae3my.png" alt="Personalized Solutions Icon">
+                        <p class="mt-2">Personalized <br> Solutions</p>
+                    </div>
+                    <div class="col-md-4 icon-text">
+                        <img src="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736931550/Energy/Rectangle_33_atqnrr.png" alt="High Performance Icon">
+                        <p class="mt-2">High <br> Performance</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Right Image -->
+            <div class="col-lg-6">
+                <img src="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736927654/Energy/image_uhpdzw.jpg" class="right-image">
+                <div class="text-white King">
+                    <h1>500,000</h1>
+                    <p>Homes and Businesses <br> to be powered by solar.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
 
 <script>
--- resources/views/Layouts/connection.blade.php
+++ resources/views/Layouts/connection.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="{{URL::asset('assets/css/bootstrap.min.css')}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Sora:wght@100..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
