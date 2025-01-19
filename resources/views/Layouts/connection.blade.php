008ea1211f04709275b342a919f7c81904d3dd18
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-01-19 14:57:39 +0100

    update

--- resources/views/Layouts/connection.blade.php
+++ resources/views/Layouts/connection.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1736857440/Energy/p_1_1_jbosii.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1737290212/Energy/Baltai_Energy_Logo_Agreed_2_1_1_pagyne.png" type="image/x-icon">
     <title> Home | Connections </title>
     <link rel="stylesheet" href="{{URL::asset('assets/css/style.css')}}">
     <link rel="stylesheet" href="{{URL::asset('assets/css/bootstrap.min.css')}}">
---
+++ resources/views/Layouts/footer.blade.php
@@ -0,0 +1,192 @@
+<!-- <section class="Newsletter">
+    <div class="container Amen">
+        <div class="row">
+            <div class="col-lg-6">
+                <p>Subscribe to our Newsletter</p>
+                <h1>Stay informed on Industry trends</h1>
+            </div>
+            <div class="col-lg-6">
+                <form class="search-bar">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="email" placeholder="Your email Address" required>
+                        <input type="submit" class="input-group-text" value="Subscribe Now" id="basic-addon2">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section> -->
+<section class="footer">
+<div class="container Amen">
+        <div class="row">
+            <div class="col-lg-6">
+                <p>Subscribe to our Newsletter</p>
+                <h1>Stay informed on Industry trends</h1>
+            </div>
+            <div class="col-lg-6">
+                <form class="search-bar">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="email" placeholder="Your email Address" required>
+                        <input type="submit" class="input-group-text" value="Subscribe Now" id="basic-addon2">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="quick">
+                    <ul>
+                        <li>
+                            <div class="force">
+                                <a href="{{route('index')}}">
+                                    <img src="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1737290216/Energy/Baltai_Energy_Logo_Agreed_2_1_stuhkw.png" draggable="false" alt="">
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            We are Solar Systems Solutions Provider based in Lagos, Nigeria; with an international team and exclusive partnerships with top European and Asian Engineering OEMs
+                        </li>
+                    </ul>
+                    <h1>
+                        Follow Us
+                    </h1>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <img src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1677678779/OjaFunnel-Images/facebook_n5uvff.png" draggable="false" title="Follow" alt="">
+                            </a>
+                            <a href="#">
+                                <img src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1677678781/OjaFunnel-Images/twitter_kd7mew.png" draggable="false" alt="">
+                            </a>
+                            <a href="#">
+                                <img src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1677678783/OjaFunnel-Images/instagram_zf1kco.png" draggable="false" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="quick">
+                    <h1>
+                        Company
+                    </h1>
+                    <ul>
+                        <li>
+                            <a href="{{route('index')}}">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Our Team</a>
+                        </li>
+                        <li>
+                            <a href="#">FAQs</a>
+                        </li>
+                        <li>
+                            <a href="#">Partners</a>
+                        </li>
+                        <li>
+                            <a href="#">Solution</a>
+                        </li>
+                        <li>
+                            <a href="#">Insights</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="quick">
+                    <h1>
+                        Quick Link
+                    </h1>
+                    <ul>
+                        <li>
+                            <a href="#">Financing</a>
+                        </li>
+                        <li>
+                            <a href="#">Partnerships</a>
+                        </li>
+                        <li>
+                            <a href="#">Energy Glossary</a>
+                        </li>
+                        <li>
+                            <a href="#">Energy Calculator</a>
+                        </li>
+                        <li>
+                            <a href="#">Apply for SSN</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="quick">
+                    <h1>
+                        Our Services
+                    </h1>
+                    <ul>
+                        <li>
+                            <a href="#">Solar Panels</a>
+                        </li>
+                        <li>
+                            <a href="#">Inverter + Battery</a>
+                        </li>
+                        <li>
+                            <a href="#">Cables, Accessories</a>
+                        </li>
+                        <li>
+                            <a href="#">Installation</a>
+                        </li>
+                        <li>
+                            <a href="#">After Sales Service</a>
+                        </li>
+                    </ul>
+                    <h1>
+                        Resources
+                    </h1>
+                    <ul>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Terms & Condition</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="quick">
+                    <h1>
+                        Contact Us
+                    </h1>
+                    <ul>
+                        <li>
+                            <a href="#"><i class="bi bi-envelope"></i> info@baltaienergy.com</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-envelope"></i> baltaienergy@gmail.com</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-phone"></i> +234-913-714-8567‬, ‪+234 913 713 1599‬</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-geo-alt"></i> 652 Lagos-Abeokuta Express Way, Alagbado, Adura bus stop, Lagos.</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="last-fott text-center">
+                    <h1>
+                        Copyright © {{ date('Y') }} <code>Global Baltai Energy</code>. All rights reserved
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
