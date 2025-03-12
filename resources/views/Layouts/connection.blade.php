f67c438a5d90674324a2102b18a1e764ff570d31
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-03-12 10:15:21 +0100

    Update

--- resources/views/Layouts/connection.blade.php
+++ resources/views/Layouts/connection.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="https://res.cloudinary.com/ddj0k8gdw/image/upload/v1737290212/Energy/Baltai_Energy_Logo_Agreed_2_1_1_pagyne.png" type="image/x-icon">
-    <title> Home | Connections </title>
+    <title>Baltai Energy | Off-Grid & On-Grid Solar Solutions for Residential, Commercial & Industrial Projects in Nigeria </title>
     <link rel="stylesheet" href="{{URL::asset('assets/css/style.css')}}">
     <link rel="stylesheet" href="{{URL::asset('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{URL::asset('assets/css/animate.min.css')}}">
